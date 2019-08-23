d23216fc36c45a4d4a3f586c304b2e68898d1a33
Keith Gable <keith@art19.com>
2019-08-23 16:57:52 -0700

    [PLAT-1] #time 5m Add support for the public IP

--- route53.tf
+++ route53.tf
@@ -5,7 +5,7 @@ resource "aws_route53_record" "this" {
   name    = "${(var.instance_count > 1) || (var.use_num_suffix == "true") ? format("%s%04d%s", var.dns_name, count.index+1, local.dns_suffix) : format("%s%s", var.dns_name, local.dns_suffix)}." # trailing dot intentional
   type    = "A"
   ttl     = "${var.dns_ttl}"
-  records = ["${aws_instance.this.*.private_ip[count.index]}"]
+  records = ["${var.dns_ip_type == "private" ? aws_instance.this.*.private_ip[count.index] : aws_instance.this.*.public_ip[count.index]}"]
 }
 
 resource "aws_route53_record" "this_t2" {
@@ -15,5 +15,5 @@ resource "aws_route53_record" "this_t2" {
   name    = "${(var.instance_count > 1) || (var.use_num_suffix == "true") ? format("%s%04d%s", var.dns_name, count.index+1, local.dns_suffix) : format("%s%s", var.dns_name, local.dns_suffix)}." # trailing dot intentional
   type    = "A"
   ttl     = "${var.dns_ttl}"
-  records = ["${aws_instance.this_t2.*.private_ip[count.index]}"]
+  records = ["${var.dns_ip_type == "private" ? aws_instance.this_t2.*.private_ip[count.index] : aws_instance.this_t2.*.public_ip[count.index]}"]
 }
